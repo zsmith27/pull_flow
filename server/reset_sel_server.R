44cbbe51e8d807740ad504c2ae67e8045ae2721c
Zachary Smith <zsmith@ICPRB.INT>
2017-08-23 16:21:18 -0400

    Update checkboxes.

--- server/reset_sel_server.R
+++ server/reset_sel_server.R
@@ -30,3 +30,42 @@ observeEvent(input$clear.usgs, {
                            selected = NULL)
 })
 #------------------------------------------------------------------------------
+usgs.gages <- reactive({
+  usgs.gages.df %>% 
+    dplyr::filter(code %in% unique(retrieved()$site)) %>% 
+    dplyr::arrange(description)
+})
+
+
+#----------------------------------------------------------------------------
+observeEvent(input$view_reset_usgs, {
+  updateCheckboxGroupInput(session, "view_gages_cbox", 
+                           selected = usgs.gages()$code)
+#                             c("conoco", "goose", "mon_jug", "barnum",
+#                                        "kitzmiller", "luke", "nbp_cumb", "opequan",
+#                                        "hanc", "paw", "por", "shepherdstown",
+#                                        "lfalls", "bloomington", "barton", 
+#                                        "seneca", "shen_mill"))
+})
+#----------------------------------------------------------------------------
+observeEvent(input$view_clear_usgs, {
+  updateCheckboxGroupInput(session, "view_gages_cbox", "USGS Gage",
+                           c("Conococheague Creek at Fairfview, MD" = "conoco",
+                             "Goose Creek near Leesburg, VA" = "goose",
+                             "Monocacy River at Jug Bridge near Frederick, MD" = "mon_jug",
+                             "North Branch Potomac River at Barnum, WV" = "barnum",
+                             "North Branch Potomac River at Kitzmiller, MD" = "kitzmiller",
+                             "North Branch Potomac River at Luke, MD" = "luke",
+                             "North Branch Potomac River near Cumberland, MD" = "nbp_cumb",
+                             "Opequon Creek near Martinsburg, WV" = "opequan",
+                             "Potomac River at Hancock, MD" = "hanc",
+                             "Potomac River at Paw Paw, WV" = "paw",
+                             "Potomac River at Point of Rocks, MD" = "por",
+                             "Potomac River at Shepherdstown, WV" = "shepherdstown",
+                             "Potomac River near Wash, DC Little Falls pump sta" = "lfalls",
+                             "Savage River Below Savage River Dam Near Bloomington, MD" = "bloomington",
+                             "Savage River Near Barton, MD" = "barton",
+                             "Seneca Creek at Dawsonville, MD" = "seneca",
+                             "Shenandoah River at Millville, WV" = "shen_mill"),
+                           selected = NULL)
+})
