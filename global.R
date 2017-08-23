1537684175530905c0a5eebe60a2667980dda0ce
Zachary Smith <zsmith@ICPRB.INT>
2017-08-23 16:15:12 -0400

    Global loads more packages, imports plotting functions, imports usgs gage list, and specifies standard plot height and width.

--- global.R
+++ global.R
@@ -2,3 +2,12 @@
 source("functions/pull_flow_func.R", local = TRUE)
 source("functions/pull_withdrawals_func.R", local = TRUE)
 source("global/load_packages.R", local = TRUE)
+source("global/usgs_plot_func.R", local = TRUE)
+#------------------------------------------------------------------------------
+file.dir <- file.path("www/potomac_gages.csv")
+usgs.gages.df <- data.table::fread(file.dir, data.table = FALSE,
+                             colClasses = list(character = c("site_no"))) %>% 
+  mutate(site_no = paste0("0", site_no))
+#------------------------------------------------------------------------------
+plot.height <- "340px"
+plot.width <- "95%"
