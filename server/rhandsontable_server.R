4eb0308d03ea2563bfb62b74f77686b4d93b1ff4
Zachary Smith <zsmith@ICPRB.INT>
2017-08-23 16:20:58 -0400

    Use rhandsontable to allow data to be editted.

--- server/rhandsontable_server.R
+++ server/rhandsontable_server.R
@@ -1,7 +1,9 @@
 output$hot <- renderRHandsontable({
   DF <- retrieved()
-  if (!is.null(DF))
+  if (!is.null(DF)) {
     rhandsontable(DF) %>% 
-    hot_table(highlightCol = TRUE, highlightRow = TRUE) %>% 
-    hot_cols(columnSorting = TRUE)
+      hot_table(highlightCol = TRUE, highlightRow = TRUE) %>% 
+      hot_cols(columnSorting = TRUE)
+  }
+
 })
