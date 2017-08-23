22d0da280f24bbbba2826047607cd17b088b569f
Zachary Smith <zsmith@ICPRB.INT>
2017-08-23 16:16:03 -0400

    Created five tabs: import, view, edit, append, and download.

--- server.R
+++ server.R
@@ -5,9 +5,21 @@ shinyServer(function(input, output, session) {
   # to console.
   session$onSessionEnded(stopApp)
   #----------------------------------------------------------------------------
+  # Tab Import
   source("server/reset_sel_server.R", local = TRUE)
   source("server/dates_server.R", local = TRUE)
   source("server/retrieve_server.R", local = TRUE)
+  #----------------------------------------------------------------------------
+  # Tab View
   source("server/download_server.R", local = TRUE)
+  #----------------------------------------------------------------------------
+  # Tab Edit
   source("server/rhandsontable_server.R", local = TRUE)
+  #----------------------------------------------------------------------------
+  # Tab Append
+  #----------------------------------------------------------------------------
+  # Tab Download
+  source("server/view_server.R", local = TRUE)
+  #----------------------------------------------------------------------------
+  #source("server/filter_server.R", local = TRUE)
 })
--- ui.R
+++ ui.R
@@ -1,10 +1,12 @@
 shinyUI(
-  fluidPage(
-    titlePanel("COOP Data Download"),
-    source("ui/sidebar_ui.R", local = TRUE)$value,
-    mainPanel(
-      rHandsontableOutput("hot")
-      #dataTableOutput('retrieve_table')
-      ) # End mainPanel
+  navbarPage("COOP",
+             id = "tab",
+             inverse = TRUE, 
+             theme = shinythemes::shinytheme("spacelab"),
+             source("ui/import_ui.R", local = TRUE)$value,
+             source("ui/view_ui.R", local = TRUE)$value,
+             source("ui/edit_ui.R", local = TRUE)$value,
+             source("ui/append_ui.R", local = TRUE)$value,
+             source("ui/download_ui.R", local = TRUE)$value
   )
 )
