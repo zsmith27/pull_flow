2e96aef0fe5181c3dd919b4f96d5b0709cb883f0
Zachary Smith <zsmith@ICPRB.INT>
2017-08-24 09:51:01 -0400

    Dropdown menus to filter drupal data.

--- ui/view_sidebar_ui.R
+++ ui/view_sidebar_ui.R
@@ -39,10 +39,26 @@ sidebarPanel(
                    hr()
   ), # End Conditional Panel usgs
   conditionalPanel(condition = "input.data_set == 'drupal'",
-                   checkboxGroupInput("view_unique_cbox",  "Unique ID",
+                   checkboxGroupInput("view_unique_cbox",  "Unique ID:",
                                       NULL),
                    actionButton("view_reset_drupal", "Reset"),
                    actionButton("view_clear_drupal", "Clear"),
+                   hr(),
+                   selectInput("view_dropdown_measurement_drupal", "Select a Measurement:",
+                               choices = c("current usable storage", "daily average demand", 
+                                           "daily average withdrawals", "daily discharge",            
+                                           "estimated average discharge", "forecasted demand",
+                                           "total usable capacity"),
+                               selected = "daily average withdrawals"
+                   ),
+                   selectInput("view_dropdown_day_drupal", "Select a Day:",
+                               choices = c("Yesterday", "Today", "Tomorrow"),
+                               selected = "Today"
+                   ),
+                   selectInput("view_dropdown_time_drupal", "Select a Time:",
+                               choices = c("AM", "PM"),
+                               selected = "AM"
+                   ),
                    hr()
   ), # End Conditional Panel drupal
   numericInput("min_flow", "Minimum Flow:",
