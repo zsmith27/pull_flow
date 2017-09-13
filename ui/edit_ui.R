1446c105b6a5f068284956923a05544102d06ebc
Zachary Smith <zsmith@ICPRB.INT>
2017-09-13 15:05:13 -0400

    Hiding edit sidebar until I can get it working.

--- ui/edit_ui.R
+++ ui/edit_ui.R
@@ -1,8 +1,8 @@
 tabPanel("Edit",
          icon = icon("edit"),
          fluidRow(
-           column(3,
-                  source("ui/edit_sidebar_ui.R", local = TRUE)$value
+           column(3#,
+                  #source("ui/edit_sidebar_ui.R", local = TRUE)$value
            ),
            column(6,
                   align = "left",
