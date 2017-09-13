84ae123cbb92bd33c1afec0219970f2fe8bd88a9
Zachary Smith <zsmith@ICPRB.INT>
2017-09-13 15:03:53 -0400

    Working on making the view checkboxes update dynamically.

--- server/reset_sel_server.R
+++ server/reset_sel_server.R
@@ -40,7 +40,7 @@ usgs.gages <- reactive({
 #----------------------------------------------------------------------------
 observeEvent(input$view_reset_usgs, {
     updateCheckboxGroupInput(session, "view_gages_cbox", 
-                             selected = usgs.gages()$code)
+                             selected = sites.vec())
     #                             c("conoco", "goose", "mon_jug", "barnum",
     #                                        "kitzmiller", "luke", "nbp_cumb", "opequan",
     #                                        "hanc", "paw", "por", "shepherdstown",
@@ -50,31 +50,15 @@ observeEvent(input$view_reset_usgs, {
 #----------------------------------------------------------------------------
 observeEvent(input$view_clear_usgs, {
   updateCheckboxGroupInput(session, "view_gages_cbox", "USGS Gage",
-                           c("Conococheague Creek at Fairfview, MD" = "conoco",
-                             "Goose Creek near Leesburg, VA" = "goose",
-                             "Monocacy River at Jug Bridge near Frederick, MD" = "mon_jug",
-                             "North Branch Potomac River at Barnum, WV" = "barnum",
-                             "North Branch Potomac River at Kitzmiller, MD" = "kitzmiller",
-                             "North Branch Potomac River at Luke, MD" = "luke",
-                             "North Branch Potomac River near Cumberland, MD" = "nbp_cumb",
-                             "Opequon Creek near Martinsburg, WV" = "opequan",
-                             "Potomac River at Hancock, MD" = "hanc",
-                             "Potomac River at Paw Paw, WV" = "paw",
-                             "Potomac River at Point of Rocks, MD" = "por",
-                             "Potomac River at Shepherdstown, WV" = "shepherdstown",
-                             "Potomac River near Wash, DC Little Falls pump sta" = "lfalls",
-                             "Savage River Below Savage River Dam Near Bloomington, MD" = "bloomington",
-                             "Savage River Near Barton, MD" = "barton",
-                             "Seneca Creek at Dawsonville, MD" = "seneca",
-                             "Shenandoah River at Millville, WV" = "shen_mill"),
+                           sites.vec(),
                            selected = NULL)
 })
 #----------------------------------------------------------------------------
-observeEvent(input$retrieve_table, {
-  updateCheckboxGroupInput(session, "view_unique_cbox", 
-                           choices = unique(retrieved()$unique_id),
-                           selected = unique(retrieved()$unique_id))
-})
+#observeEvent(input$retrieve_table, {
+#  updateCheckboxGroupInput(session, "view_unique_cbox", 
+#                           choices = unique(retrieved()$unique_id),
+#                           selected = unique(retrieved()$unique_id))
+#})
 #----------------------------------------------------------------------------
 observeEvent(input$view_reset_drupal, {
   updateCheckboxGroupInput(session, "view_unique_cbox", 
