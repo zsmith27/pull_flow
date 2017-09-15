47b016d08264ee7492d7493c17bbf05abcf51e0e
Zachary Smith <zsmith@icprb.int>
2017-09-15 07:45:38 -0400

    The usgs pull_flow function now only converts "iv" data to EST. Previously subtracting five hours from the daily data was leading to confusing results.

--- functions/pull_flow_func.R
+++ functions/pull_flow_func.R
@@ -17,14 +17,21 @@ retrieve_flow <- function(gage = NULL, start.date = "1950-10-30",
   #--------------------------------------------------------------------------
   names(gage.df)[4:5] <- c("discharge_cfs", "qual_code")
   #--------------------------------------------------------------------------
+  if (service.type == "iv") {
+    gage.df <- gage.df %>% 
+      dplyr::mutate(dateTime = as.POSIXct(dateTime, format = "%Y-%m-%dT%H:%M"),
+             dateTime = as.POSIXct(round(dateTime, units = "hours")),
+             # Subtract 5 hours to convert UTC to EST.
+             dateTime = dateTime - lubridate::hours(5),
+             tz_cd = "EST") %>% 
+      dplyr::filter(dateTime >= start.date)
+  }
+
+  #--------------------------------------------------------------------------
   final.df <- gage.df %>% 
     rename(agency = agency_cd,
            site = site_no, 
            timezone = tz_cd) %>% 
-    mutate(dateTime = as.POSIXct(dateTime, format = "%Y-%m-%dT%H:%M"),
-           dateTime = as.POSIXct(round(dateTime, units = "hours")),
-           # Subtract 5 hours to convert UTC to EST.
-           dateTime = dateTime - lubridate::hours(5)) %>% 
     group_by(agency, site, dateTime, qual_code, timezone) %>% 
     summarize(flow = mean(discharge_cfs)) %>% 
     ungroup(dateTime) %>%
