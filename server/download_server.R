70f1506e5dc3ae284e34f38cff34679723066491
zsmith27 <zsmith@icprb.org>
2017-09-23 16:24:00 -0400

    Export faster and keep date format.

--- server/download_server.R
+++ server/download_server.R
@@ -2,7 +2,8 @@ output$downloadData <- downloadHandler(
   filename = function() { paste0(input$file_name, '.csv') },
   content = function(file) {
     #write.csv(retrieved(), file, row.names = FALSE)
-    write.csv(final.df(), file, row.names = FALSE)
+    data.table::fwrite(final.df(), file)
+    #write.csv(final.df(), file, row.names = FALSE)
   }
 )
 #------------------------------------------------------------------------------
