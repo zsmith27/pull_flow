c2506ba9f53c7099d595d831f316e3c1b303ac3e
zsmith27 <zsmith@icprb.org>
2018-07-18 07:22:23 -0400

    Hide the append tab it is not ready.

--- ui.R
+++ ui.R
@@ -1,13 +1,13 @@
 shinyUI(
   navbarPage(title = tags$a("", href = "http://icprbcoop.org/drupal4/self-breifing-conditions", target = "_blank",
-                            tags$span(style="color:white", "CO-OP")),
+                            tags$span(style = "color:white", "CO-OP")),
              id = "tab",
              inverse = TRUE, 
              theme = shinythemes::shinytheme("spacelab"),
              source("ui/import_ui.R", local = TRUE)$value,
              source("ui/view_ui.R", local = TRUE)$value,
              source("ui/edit_ui.R", local = TRUE)$value,
-#             source("ui/append_ui.R", local = TRUE)$value,
+             # source("ui/append_ui.R", local = TRUE)$value,
              source("ui/download_ui.R", local = TRUE)$value
   )
 )
